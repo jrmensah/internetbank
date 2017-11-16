21ac07ee32fe8c2c7b2271c9536d7a5512297935
jrmensah <jerilynmensah@gmail.com>
2017-11-16 09:53:30 -0500

    Internet Security with Security config.

---
+++ src/main/java/me/jrmensah/internetbank/MainController.java
@@ -0,0 +1,22 @@
+package me.jrmensah.internetbank;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/login")
+        public String login(){
+            return "login";
+        }
+    @RequestMapping("/register")
+    public String register(){
+            return "register";
+    }
+}
